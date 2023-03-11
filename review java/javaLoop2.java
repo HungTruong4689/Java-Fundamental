51377135507d9f1a59e67d4280d0f11d665bfcbe
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-11 20:27:19 +0200

    loop value of data

--- review java/javaLoop2.java
+++ review java/javaLoop2.java
@@ -4,21 +4,24 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
         Scanner myObj = new Scanner(System.in);
         int N = myObj.nextInt();
-        for(int i =0;i<N ;i++){
+        // loop value of data
+        for (int i = 0; i < N; i++) {
             int a = myObj.nextInt();
             int b = myObj.nextInt();
             int n = myObj.nextInt();
-            int temp=a;
-            for(int j =0;j<n;j++){
-                temp = temp + (int)Math.pow(2, j) *b;
-                System.out.print(temp+ " ");
+            int temp = a;
+            for (int j = 0; j < n; j++) {
+                temp = temp + (int) Math.pow(2, j) * b;
+                System.out.print(temp + " ");
             }
             System.out.println("");
         }
-        
-        
+
     }
 }
