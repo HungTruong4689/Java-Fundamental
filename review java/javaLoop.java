39c570b1a160119000a9b5196eb090c8a8245ca3
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-11 20:26:02 +0200

    show data loop

--- review java/javaLoop.java
+++ review java/javaLoop.java
@@ -10,20 +10,15 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-
-
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
-        for(int i =1; i<11;i++){
-            System.out.println( N + " x " + i + " = "+i*N);
+        for (int i = 1; i < 11; i++) {
+            System.out.println(N + " x " + i + " = " + i * N);
         }
 
         bufferedReader.close();
     }
-}
-public class javaLoop {
-    
 }
