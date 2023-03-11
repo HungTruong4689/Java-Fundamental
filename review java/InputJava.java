9886169043c6b18c8e6da0e7f8a3575d6be94f2b
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-11 20:23:28 +0200

    show input of data

--- review java/InputJava.java
+++ review java/InputJava.java
@@ -4,14 +4,18 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
         Scanner myObj = new Scanner(System.in);
         int age = myObj.nextInt();
         double salary = myObj.nextDouble();
         myObj.nextLine();
+        // show input of data
         String name = myObj.nextLine();
-        System.out.println("String: "+name);
-        System.out.println("Double: "+salary);
-        System.out.println("Int: "+age);
+        System.out.println("String: " + name);
+        System.out.println("Double: " + salary);
+        System.out.println("Int: " + age);
     }
 }
