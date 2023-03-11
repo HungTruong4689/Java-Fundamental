9b4b88ce57ec1df076341699ba25a626170eb21f
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-11 20:25:00 +0200

    show bigInteger of data

--- review java/javaBigInteger.java
+++ review java/javaBigInteger.java
@@ -7,11 +7,15 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc=new Scanner(System.in);
-    BigInteger a=new BigInteger(sc.nextLine());
-    BigInteger b=new BigInteger(sc.nextLine());
-    System.out.println(a.add(b));
-    System.out.println(a.multiply(b));
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = new BigInteger(sc.nextLine());
+        BigInteger b = new BigInteger(sc.nextLine());
+        // calculate the data
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
     }
 }
