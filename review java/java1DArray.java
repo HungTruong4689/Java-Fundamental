d69b29328387a061133eb202f2083222e6c2be6e
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-11 20:24:17 +0200

    show command of data

--- review java/java1DArray.java
+++ review java/java1DArray.java
@@ -10,30 +10,31 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-
-
 public class Solution {
     public static void main(String[] args) throws IOException {
-        // BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader bufferedReader = new BufferedReader(new
+        // InputStreamReader(System.in));
 
         // int n = Integer.parseInt(bufferedReader.readLine().trim());
-        
-        // List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-        //     .map(Integer::parseInt)
-        //     .collect(toList());
+
+        // List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$",
+        // "").split(" "))
+        // .map(Integer::parseInt)
+        // .collect(toList());
 
         // bufferedReader.close();
         Scanner scan = new Scanner(System.in);
-    int n = scan.nextInt();
-    int[] a=new int[n];
-    for(int i=0;i<n;i++){
-        a[i]=scan.nextInt();
-    }
-    scan.close();
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
 
-    // Prints each sequential element in array a
-    for (int i = 0; i < a.length; i++) {
-        System.out.println(a[i]);
-    }
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            // show command of data
+            System.out.println(a[i]);
+        }
     }
 }
