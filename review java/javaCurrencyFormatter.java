0f83cc350b58967c0e0e7beef2fc5f5b0814d3ca
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-11 20:25:33 +0200

    show data

--- review java/javaCurrencyFormatter.java
+++ review java/javaCurrencyFormatter.java
@@ -6,17 +6,20 @@ public class Solution {
     static Locale india = new Locale("en", "IN");
     static Locale china = Locale.CHINA;
     static Locale france = Locale.FRANCE;
-    static String convert(double amount , Locale lo){
+
+    static String convert(double amount, Locale lo) {
         NumberFormat n = NumberFormat.getCurrencyInstance(lo);
         return n.format(amount);
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double payment = scanner.nextDouble();
         scanner.close();
-        System.out.println("US: " + convert(payment,us));
-        System.out.println("India: " + convert(payment,india));
-        System.out.println("China: " + convert(payment,china));
-        System.out.println("France: " + convert(payment,france));
+        // format type of data
+        System.out.println("US: " + convert(payment, us));
+        System.out.println("India: " + convert(payment, india));
+        System.out.println("China: " + convert(payment, china));
+        System.out.println("France: " + convert(payment, france));
     }
 }
