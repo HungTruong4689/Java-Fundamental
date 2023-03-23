7f1b1af0d6453cbe8da1a8d6d2f0583ddf019b3b
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-23 18:58:32 +0200

    children class

--- OOP part 1/Inheritance/Dog.java
+++ OOP part 1/Inheritance/Dog.java
@@ -1,12 +1,15 @@
 package Inheritance;
-public class Dog extends Animal{
+
+public class Dog extends Animal {
     private int eyes;
     private int legs;
     private int tail;
     private int teeth;
     private String coat;
-    public Dog(String name, int size, int weight, int eyes,int legs, int tail, int teeth, String coat){
-        super(name, 1,1, size, weight);
+
+    // children class
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
         this.eyes = eyes;
         this.legs = legs;
         this.tail = tail;
@@ -14,29 +17,33 @@ public class Dog extends Animal{
         this.coat = coat;
     }
 
-    private void chew(){
+    private void chew() {
         System.out.println("Dog.chew() called");
     }
+
     @Override
-    public void eat(){
+    public void eat() {
         System.err.println("Dog.eat() called");
         chew();
         super.eat();
     }
 
-    public void walk(){
+    public void walk() {
         System.out.println("Dog.walk() called");
         super.move(5);
     }
-    public void run(){
+
+    public void run() {
         System.out.println("Dog.run() called");
         move(10);
     }
-    private void moveLegs( int speed){
+
+    private void moveLegs(int speed) {
         System.out.println("Dog.moveLegs() called");
     }
+
     @Override
-    public void move(int speed){
+    public void move(int speed) {
         System.out.println("Dog.move() called");
         moveLegs(speed);
         super.move(speed);
