88887831941bbef74a739ae8dc5d3139669f41e1
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-23 18:55:16 +0200

    set initial constructor

--- OOP part 1/Constructor/Account.java
+++ OOP part 1/Constructor/Account.java
@@ -1,4 +1,5 @@
 package Constructor;
+
 public class Account {
     private String number;
     private double balance;
@@ -6,64 +7,72 @@ public class Account {
     private String customerEmailAddress;
     private String customerPhoneNumber;
 
-    public Account(){
-        this("56789",2.50,"Default Name", "Default Address", "Default Phone");
+    // plain constructor
+    public Account() {
+        this("56789", 2.50, "Default Name", "Default Address", "Default Phone");
         System.out.println("Empty constructor created!");
     }
-    public Account(String number, double balance, String customerName, String customerEmailAddress, String customerPhoneNumber){
+
+    public Account(String number, double balance, String customerName, String customerEmailAddress,
+            String customerPhoneNumber) {
         this.number = number;
         this.balance = balance;
         this.customerName = customerName;
         this.customerEmailAddress = customerEmailAddress;
         this.customerPhoneNumber = customerPhoneNumber;
     }
-    public void deposit(double depositAmount){
+
+    public void deposit(double depositAmount) {
         this.balance += depositAmount;
-        System.out.println("Deposit of "+ depositAmount +" made. New balance is "+ this.balance);
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.balance);
     }
-    public void withdrawal(double withdrawalAmount){
-        if(balance - withdrawalAmount <=0){
-            System.out.println("Only "+balance + " available. Withdrawal not processed");
-        }else{
+
+    public void withdrawal(double withdrawalAmount) {
+        if (balance - withdrawalAmount <= 0) {
+            System.out.println("Only " + balance + " available. Withdrawal not processed");
+        } else {
             balance -= withdrawalAmount;
-            System.out.println("Withdrawal of "+ withdrawalAmount + " processed. Remaining balance");
+            System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance");
         }
     }
 
-    public String getNumber(){
+    public String getNumber() {
         return number;
     }
 
-    public void setNumber(String number){
+    public void setNumber(String number) {
         this.number = number;
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance){
+    public void setBalance(double balance) {
         this.balance = balance;
     }
-    public void setCustomerName(String customerName){
+
+    public void setCustomerName(String customerName) {
         this.customerName = customerName;
     }
-    public String getCustomerName(){
+
+    public String getCustomerName() {
         return customerName;
     }
-    public String getCustomerEmailAddress(){
+
+    public String getCustomerEmailAddress() {
         return customerEmailAddress;
     }
 
-    public void setCustomerEmailAddress(String customerEmailAddress){
+    public void setCustomerEmailAddress(String customerEmailAddress) {
         this.customerEmailAddress = customerEmailAddress;
     }
 
-    public String getCustomerPhoneNumber(){
+    public String getCustomerPhoneNumber() {
         return customerPhoneNumber;
     }
 
-    public void setCustomerPhoneNumber(String customerPhoneNumber){
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
         this.customerPhoneNumber = customerPhoneNumber;
     }
 }
