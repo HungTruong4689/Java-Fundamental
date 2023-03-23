23c3934c80129baf5b597f475c8aa94022e32a03
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-23 19:00:09 +0200

    super

--- OOP part 1/Inheritance/Dog.java
+++ OOP part 1/Inheritance/Dog.java
@@ -9,6 +9,7 @@ public class Dog extends Animal {
 
     // children class
     public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        // super to take value from parent
         super(name, 1, 1, size, weight);
         this.eyes = eyes;
         this.legs = legs;
