b9bacb68bfb92cee8b68447862a1501ef455d79c
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-23 12:13:44 +0200

    using the method of the class

--- OOP part 1/Class/LearningClass.java
+++ OOP part 1/Class/LearningClass.java
@@ -5,6 +5,7 @@ public class LearningClass {
         // create a new class java
         Car porsche = new Car();
         Car holden = new Car();
+        // using the method of the class
         porsche.setModel("Carrera");
         System.out.println("Model is " + porsche.getModel());
     }
