548ced7dc3432e51605ba0f3f5b1d2f94a1e12c8
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-23 12:12:57 +0200

    oop1 creating the class

--- OOP part 1/Class/LearningClass.java
+++ OOP part 1/Class/LearningClass.java
@@ -1,11 +1,11 @@
 package Class;
 
-
 public class LearningClass {
     public static void main(String[] args) {
+        // create a new class java
         Car porsche = new Car();
         Car holden = new Car();
         porsche.setModel("Carrera");
-        System.out.println("Model is "+ porsche.getModel());
+        System.out.println("Model is " + porsche.getModel());
     }
 }
