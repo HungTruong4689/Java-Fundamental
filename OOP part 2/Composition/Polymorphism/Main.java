1e5745029840e225e0953cb79aeb4777f8a07aac
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-23 19:04:08 +0200

    polymorphism

--- OOP part 2/Composition/Polymorphism/Main.java
+++ OOP part 2/Composition/Polymorphism/Main.java
@@ -1,78 +1,92 @@
 package Polymorphism;
 
-class Movie{
+//main class
+class Movie {
     private String name;
 
-    public Movie(String name){
+    public Movie(String name) {
         this.name = name;
     }
-    public String plot(){
+
+    public String plot() {
         return "No plot here";
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
 }
-class Jaws extends Movie{
-    public Jaws(){
+
+class Jaws extends Movie {
+    public Jaws() {
         super("Jaws");
     }
-    public String plot(){
+
+    public String plot() {
         return "A shark eats lots of people";
     }
 }
 
-class IndependenceDay extends Movie{
-    public IndependenceDay(){
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
         super("Independence day");
     }
+
     @Override
-    public String plot(){
+    public String plot() {
         return "Aliens attempt to take over planet earth";
     }
 }
-class StarWars extends Movie{
-    public StarWars(){
+
+class StarWars extends Movie {
+    public StarWars() {
         super("Star Wars");
     }
+
     @Override
-    public String plot(){
+    public String plot() {
         return "Imperial Forces try to take over the universe";
     }
 }
-class MazeRunner extends Movie{
-    public MazeRunner(){
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
         super("Maze Runner");
     }
+
     @Override
-    public String plot(){
+    public String plot() {
         return "Kids try and escape a maze";
     }
 }
-class Forgetable extends Movie{
-    public Forgetable(){
+
+class Forgetable extends Movie {
+    public Forgetable() {
         super("Forgetable");
     }
-    //No plot methods
+    // No plot methods
 }
+
 public class Main {
     public static void main(String[] args) {
-        //write your code here
+        // write your code here
         for (int i = 1; i < 11; i++) {
             Movie movie = randomMovie();
-            System.out.println("MOvie #"+i+" : "+ movie.getName()+"\n"+
-                                "Plot: "+ movie.plot()+ "\n");
+            System.out.println("MOvie #" + i + " : " + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() + "\n");
         }
-        
+
     }
-    public static Movie randomMovie(){
-        int randomMovie = (int) (Math.random()*5)+1;
-        System.out.println("Random number generated was: "+ randomMovie);
-        switch (randomMovie){
+
+    public static Movie randomMovie() {
+        int randomMovie = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was: " + randomMovie);
+        switch (randomMovie) {
             case 1:
                 return new Jaws();
             case 2:
